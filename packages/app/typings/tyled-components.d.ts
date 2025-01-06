26bbb486efa77bfb69f52e25e29ffe0ad315e7d1
Wii <bitly.dex@gmail.com>
2025-01-06 15:51:35 +0800

    fix: compile error

---
+++ packages/app/typings/tyled-components.d.ts
@@ -0,0 +1,4 @@
+import { ThemeInterface } from 'styles/theme'
+declare module 'styled-components' {
+	interface DefaultTheme extends ThemeInterface {}
+}
---
+++ packages/app/typings/window.d.ts
@@ -0,0 +1,25 @@
+import { ethers } from 'ethers'
+
+type NetworkId = Number
+
+declare global {
+	interface Window {
+		web3?: {
+			eth?: {
+				net: {
+					getId: () => NetworkId
+				}
+			}
+			version: {
+				getNetwork(cb: (err: Error | undefined, networkId: NetworkId) => void): void
+				network: NetworkId
+			}
+		}
+		ethereum?: {
+			on: (event: string, cb: () => void) => void
+			ethereum: ethers.providers.Provider | undefined
+			networkVersion: NetworkId
+			isMetaMask: boolean
+		}
+	}
+}
