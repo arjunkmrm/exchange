55eb044d9dd9e8ee87c5a3138603d0ac6993661f
Wii <bitly.dex@gmail.com>
2025-03-08 01:34:38 +0800

    chore: add GA code

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -10,12 +10,32 @@ import Hero from 'sections/homepage/Hero'
 import TradeNow from 'sections/homepage/TradeNow'
 import HomeLayout from 'sections/shared/Layout/HomeLayout'
 import media from 'styles/media'
+import Script from 'next/script'
 
 type AppLayoutProps = {
 	children: React.ReactNode
 }
 
 type HomePageComponent = FC & { layout?: FC<AppLayoutProps> }
+
+const GA = () => {
+	return (
+		<>
+			<Script
+				strategy="afterInteractive"
+				src="https://www.googletagmanager.com/gtag/js?id=G-DZN0CJ60CB"
+			/>
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', 'G-DZN0CJ60CB');
+				`}
+			</Script>
+		</>
+	)
+}
 
 const HomePage: HomePageComponent = () => {
 	const { t } = useTranslation()
@@ -25,6 +45,7 @@ const HomePage: HomePageComponent = () => {
 			<Head>
 				<title>{t('homepage.page-title')}</title>
 			</Head>
+			<GA />
 			<HomeLayout>
 				<Container>
 					<Hero />
